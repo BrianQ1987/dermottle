478e8282fcd7f3c926f4dd3c2032955ba39f2dac
brianq-nisra <brian.quinn@nisra.gov.uk>
2025-01-13 16:02:15 +0000

    Remove unreleased movies

--- fetch.R
+++ fetch.R
@@ -17,15 +17,17 @@ actor_info <-list()
 for (id in actor_ids) {
   tryCatch({
     details <- GET(paste0(api_base_url, "person/", id, "?api_key=", api_key))
-    actor_info[[content(details, as = "parsed")$name]] <- content(details, as = "parsed")
+    actor_name <- content(details, as = "parsed")$name
+    actor_info[[actor_name]] <- content(details, as = "parsed")
     
     credits <- GET(paste0(api_base_url, "person/", id, "/movie_credits?api_key=", api_key))
-    actor_data[[content(details, as = "parsed")$name]] <- content(credits, as = "parsed")
+    actor_data[[actor_name]] <- content(credits, as = "parsed")
   }, error = function(e) {
     cat("Error fetching actor details:\n", e$message, "\n")
   })
   
-  actor_info[[content(details, as = "parsed")$name]]$profile_path <- paste0(image_base_url, actor_info[[content(details, as = "parsed")$name]]$profile_path)
+  actor_info[[actor_name]]$profile_path <- paste0(image_base_url, actor_info[[actor_name]]$profile_path)
+  actor_info[[actor_name]]$popularity <- NULL
 }
 
 write_json(actor_info, "actors.json", auto_unbox = TRUE, pretty = TRUE)
@@ -88,30 +90,34 @@ for (actor in names(actor_data)) {
       movie$overview <- gsub(surname, "*****", movie$overview)
     }
     
-    movie_data[[as.character(movie$id)]] <- list(title = movie$title,
-                                                 answer = actor,
-                                                 character = movie$character,
-                                                 poster_path = paste0(image_base_url, movie$poster_path),
-                                                 release_date = substr(movie$release_date, 1, 4),
-                                                 overview = movie$overview,
-                                                 director = director,
-                                                 co_star_1 = co_star_1,
-                                                 co_star_2 = co_star_2,
-                                                 imdb = movie_info$imdb_id)
+    if (movie$release_date != "") {
+      movie_data[[as.character(movie$id)]] <- list(
+        title = movie$title,
+        answer = actor,
+        character = movie$character,
+        poster_path = paste0(image_base_url, movie$poster_path),
+        release_date = substr(movie$release_date, 1, 4),
+        overview = movie$overview,
+        director = director,
+        co_star_1 = co_star_1,
+        co_star_2 = co_star_2,
+        imdb = movie_info$imdb_id
+      )
+    }
+  
   }
 }
 
 # Write the processed movies to a new JSON file
 write_json(movie_data, "movies.json", auto_unbox = TRUE, pretty = TRUE)
 
-
 # Read picks from a JSON file
 tryCatch({
   picks <- fromJSON("picks.json")
 }, error = function(e) {
-  cat("Error loading ansers.json:\n", e$message, "\n")
+  cat("Error loading picks.json:\n", e$message, "\n")
 })
 
-picks[[as.character(today() + 2)]] <- names(movie_data[sample(1:length(movie_data), size = 1)])
+picks[[as.character(today() + 1)]] <- sample(names(movie_data), 1)
 
 write_json(picks, "picks.json", auto_unbox = TRUE, pretty = TRUE)
